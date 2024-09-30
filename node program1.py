578bb4e532e8d782410d1e28207dbb20a73595ea
Shreyakankane <161017243+Shreyakankane@users.noreply.github.com>
2024-09-30 14:11:56 +0530

    Create node program1.py

---
+++ node program1.py
@@ -0,0 +1,13 @@
+def isValid(s: str) -> bool:
+    stack = []
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in bracket_map.values():
+            stack.append(char)
+        elif char in bracket_map.keys():
+            if not stack or stack.pop() != bracket_map[char]:
+                return False
+            
+    return not stack
+
